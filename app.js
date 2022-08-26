70ced1d548fe4e75200000f36c75339f94401ca1
RoninXYZ <xyzronin97@gmail.com>
2022-08-27 06:20:58 +0700

    tambah chalk

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var validator = require('validator');
+const validator = require('validator');
+const chalk = require('chalk')
+//console.log(validator.isEmail("sandika@gmail.com"));
+//console.log(validator.isMobilePhone('08112345678' , 'id-ID'));
+//console.log(validator.isNumeric('08112345678a' ));
 
-console.log(validator.isEmail("sandika@gmail.com"));
-console.log(validator.isMobilePhone('08112345678' , 'id-ID'));
-console.log(validator.isNumeric('08112345678a' ))
+
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue("Hello Aliens"));
