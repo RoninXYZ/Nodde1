5a3364a9d838ce753f5f8f41cecff8e87a29a08e
RoninXYZ <xyzronin97@gmail.com>
2022-08-27 06:04:51 +0700

    lanjut ke chalk

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var validator = require('validator');
 
 console.log(validator.isEmail("sandika@gmail.com"));
+console.log(validator.isMobilePhone('08112345678' , 'id-ID'));
+console.log(validator.isNumeric('08112345678a' ))
