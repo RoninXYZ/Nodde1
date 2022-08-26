d0f3997b8a6755fef44b28416c518830ff2433ca
RoninXYZ <xyzronin97@gmail.com>
2022-08-26 12:22:29 +0700

    bosen jenuh

--- alfa1.js
+++ alfa1.js
@@ -7,5 +7,15 @@ function CetakNama(nama){
 
 const PI = 3.14;
 
+
+const mahasiswa = {
+    nama : "Dody Ferdiansyah ",
+    no : 494,
+    cetakMhs(){
+        return `Hello , Nama saya ${this.nama} dan saya no ${this.no}`
+    }
+}
+
+
 module.exports.CetakNama = CetakNama;
 module.exports.PI = PI;
