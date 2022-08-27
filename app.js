8e8e0bdbb500038860618348f84335d6406ab85c
RoninXYZ <xyzronin97@gmail.com>
2022-08-28 06:42:13 +0700

    nambah req res

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const http = require('http');
 const port = 3000;
 http
     .createServer((req, res) => {
-        const url = req.url;
-        console.log(url);
         res.writeHead(200,{'Content-Type':'text/html'})
-        res.write('Hello Aliens');
-        res.end();
+        const url = req.url;
+        if(url === '/about'){
+            res.write('<h1>ini halaman About</h1>');
+            res.end();
+        }
+        else if(url === '/contac'){
+            res.write('<h1>ini halaman contac</h1>');
+            res.end();
+        }else{
+            res.write('<h1>Hello Aliens</h1>');
+            res.end(); 
+        }
+
+        
+       
+        // res.write('Hello Aliens');
+        // res.end();
     })
     .listen(port, () => {
         console.log(`listening in port ${port}`);
