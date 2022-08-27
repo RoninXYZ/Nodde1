b3a19aedb80b73219c4f9bfde2cb63fb44680f2e
RoninXYZ <xyzronin97@gmail.com>
2022-08-27 10:11:21 +0700

    berhasil

--- app.js
+++ app.js
@@ -1,26 +1,81 @@
+const { rejects } = require('assert');
 const fs = require('fs');
+const { resolve } = require('path');
 const readline = require('readline');
 const rl = readline.createInterface({
     input:process.stdin,
     output:process.stdout,
 })
-rl.question('masukan nama anda?? ', (nama) => {
-    // TODO: Log the answer in a database
-    rl.question('masukan nomor hp anda??' ,(noHp)=>{
-      console.log(`Terimakasih ${nama} no hp anda ${noHp}`)
-      const kontak = {
-        nama ,//:nama,
-        noHp //: noHp 
-      }
-     const fileBuffer =  fs.readFileSync("data/contact.json" , 'utf-8');
-     const contact1 = JSON.parse(fileBuffer);
-    contact1.push(kontak)
-    fs.writeFileSync('data/contact.json' , JSON.stringify(contact1) ) 
+//membuat folder data
+const dirPath = "./data";
+if(!fs.existsSync(dirPath)){
+  fs.mkdirSync(dirPath);
 
-     console.log("terimakasih")
+}
+//membuat file contac .json
+const dataPath = "./data/contacts.json"
+if(!fs.existsSync(dataPath)){
+  fs.writeFileSync(dataPath,"[]" , "utf-8");
 
-      rl.close();
-    })
+}
+
+const pertanyaan1 = ()=>{
+  return new Promise((resolve , rejects)=>{
+    rl.question('masukan nama anda?? ', (nama) => {
+      resolve(nama);
+    });
+  });
+}
+
+
+
+const pertanyaan2 = ()=>{
+  return new Promise((resolve , reject)=>{
+    rl.question('masukan email anda?? ', (email) => {
+      resolve(email);
+    });
+  });
+}
+
+const main =async ()=>{
+  const nama = await pertanyaan1();
+  const email = await pertanyaan2();
+  const kontak = {
+            nama ,//:nama,
+            email //: noHp 
+          }
+         const fileBuffer =  fs.readFileSync("data/contacts.json" , 'utf-8');
+         const contact1 = JSON.parse(fileBuffer);
+        contact1.push(kontak)
+        fs.writeFileSync('data/contacts.json' , JSON.stringify(contact1) ) 
+    
+         console.log("terimakasih")
+    
+          rl.close();
+ }
+main();
+
+
+
+
+
+// //rl.question('masukan nama anda?? ', (nama) => {
+//     // TODO: Log the answer in a database
+// //    rl.question('masukan nomor hp anda??' ,(noHp)=>{
+//  //     console.log(`Terimakasih ${nama} no hp anda ${noHp}`)
+//       const kontak = {
+//         nama ,//:nama,
+//         noHp //: noHp 
+//       }
+//      const fileBuffer =  fs.readFileSync("data/contacts.json" , 'utf-8');
+//      const contact1 = JSON.parse(fileBuffer);
+//     contact1.push(kontak)
+//     fs.writeFileSync('data/contacts.json' , JSON.stringify(contact1) ) 
+
+//      console.log("terimakasih")
+
+//       rl.close();
+//     })
 
    
-  });
+//   });
