e602f728402a930666cba567a21fafc6ccd1cd6b
RoninXYZ <xyzronin97@gmail.com>
2022-08-28 14:45:44 +0700

    tambah ejs

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const  expressLayouts = require('express-ejs-layouts');
+app.set('view engine' ,'ejs');
+app.use(expressLayouts)
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', { root: __dirname })
-
-
-
-    //   res.json({
-    //     nama :"sandika",
-    //     email :"sandika@gmail.com",
-    //     noHP : 0812345456,
-    //   })
+    //res.sendFile('./index.html', { root: __dirname }) 
+    const mahasiswa = [
+        {
+            nama:"Sandika galih",
+            email :"sandikagalih@gmail.com"
+        },
+        {
+            nama:"Erik",
+            email :"Erik@gmail.com"
+        },
+        {
+            nama:"Dody",
+            email :"Dody@gmail.com"
+        }
+    ]
+    res.render('index' ,{
+        nama : "Sandika Galih",
+        title :"Halaman Home" ,
+        mahasiswa:mahasiswa,
+        layout:'layout/main_layout',
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile('./about.html', { root: __dirname })
+    //res.sendFile('./about.html', { root: __dirname })
+    res.render('about' , {layout:'layout/main_layout',title :"Halaman about" ,})
 })
 app.get('/kontak', (req, res) => {
     //res.send('ini halaman kontak')
-    res.sendFile('./contac.html', { root: __dirname })
+    //res.sendFile('./contac.html', { root: __dirname })
+    res.render('contac' , {layout:'layout/main_layout',title :"Halaman kontak" ,})
 })
 
 app.get('/produk/:id' , (req,res)=>{
@@ -42,86 +59,3 @@ app.listen(port, () => {
 
 
 
-// const fs = require('fs');
-// const http = require('http');
-// const port = 3000;
-
-// const RenderHtml = (path, res) => {
-//     fs.readFile(path, (err, data) => {
-//         if (err) {
-//             res.writeHead(404);
-//             res.write('Eror : file not found')
-//         } else {
-//             res.write(data)
-//         }
-//         res.end();
-//     });
-// }
-
-
-
-
-// http
-//     .createServer((req, res) => {
-//         res.writeHead(200, { 'Content-Type': 'text/html' })
-//         const url = req.url;
-//         switch (url) {
-//             case '/about':
-//                 RenderHtml('about.html', res);
-//                 break;
-//             case '/contac':
-//                 RenderHtml('contac.html', res);
-//                 break;
-//             default:
-//                 RenderHtml('index.html' , res);
-//                 break;
-//         }
-
-
-
-
-
-
-
-
-//         // if(url === '/about'){
-//         //     RenderHtml('about.html' , res);
-//         //     // fs.readFile('about.html' , (err,data)=>{
-//         //     //     if(err){
-//         //     //         res.writeHead(404);
-//         //     //         res.write('Eror : file not found')
-//         //     //     }else{
-//         //     //         res.write(data)
-//         //     //     }
-//         //     //     res.end();
-//         //     //    });
-//         //     // res.write('<h1>ini halaman About</h1>');
-//         //     // res.end();
-//         // }
-//         // else if(url === '/contac'){
-//         //     RenderHtml('contac.html' , res)
-//         // }else{
-
-//         // RenderHtml('index.html' , res);
-
-
-//         // //    fs.readFile('index.html' , (err,data)=>{
-//         // //     if(err){
-//         // //         res.writeHead(404);
-//         // //         res.write('Eror : file not found')
-//         // //     }else{
-//         // //         res.write(data)
-//         // //     }
-//         // //     res.end();
-//         // //    });
-
-//         // }
-
-
-
-//         // res.write('Hello Aliens');
-//         // res.end();
-//     })
-//     .listen(port, () => {
-//         console.log(`listening in port ${port}`);
-//     })
