503b341937a3a9d1cd0069aa1dd1a2dab3e7c86a
RoninXYZ <xyzronin97@gmail.com>
2022-08-28 07:27:17 +0700

    server

--- app.js
+++ app.js
@@ -1,41 +1,80 @@
 const fs = require('fs');
 const http = require('http');
 const port = 3000;
+
+const RenderHtml = (path, res) => {
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.write('Eror : file not found')
+        } else {
+            res.write(data)
+        }
+        res.end();
+    });
+}
+
+
+
+
 http
     .createServer((req, res) => {
-        res.writeHead(200,{'Content-Type':'text/html'})
+        res.writeHead(200, { 'Content-Type': 'text/html' })
         const url = req.url;
-        if(url === '/about'){
-            fs.readFile('about.html' , (err,data)=>{
-                if(err){
-                    res.writeHead(404);
-                    res.write('Eror : file not found')
-                }else{
-                    res.write(data)
-                }
-                res.end();
-               });
-            // res.write('<h1>ini halaman About</h1>');
-            // res.end();
-        }
-        else if(url === '/contac'){
-            res.write('<h1>ini halaman contac</h1>');
-            res.end();
-        }else{
-           fs.readFile('index.html' , (err,data)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Eror : file not found')
-            }else{
-                res.write(data)
-            }
-            res.end();
-           });
-             
+        switch (url) {
+            case '/about':
+                RenderHtml('about.html', res);
+                break;
+            case '/contac':
+                RenderHtml('contac.html', res);
+                break;
+            default:
+                RenderHtml('index.html' , res);
+                break;
         }
 
-        
-       
+
+
+
+
+
+
+
+        // if(url === '/about'){
+        //     RenderHtml('about.html' , res);
+        //     // fs.readFile('about.html' , (err,data)=>{
+        //     //     if(err){
+        //     //         res.writeHead(404);
+        //     //         res.write('Eror : file not found')
+        //     //     }else{
+        //     //         res.write(data)
+        //     //     }
+        //     //     res.end();
+        //     //    });
+        //     // res.write('<h1>ini halaman About</h1>');
+        //     // res.end();
+        // }
+        // else if(url === '/contac'){
+        //     RenderHtml('contac.html' , res)
+        // }else{
+
+        // RenderHtml('index.html' , res);
+
+
+        // //    fs.readFile('index.html' , (err,data)=>{
+        // //     if(err){
+        // //         res.writeHead(404);
+        // //         res.write('Eror : file not found')
+        // //     }else{
+        // //         res.write(data)
+        // //     }
+        // //     res.end();
+        // //    });
+
+        // }
+
+
+
         // res.write('Hello Aliens');
         // res.end();
     })
