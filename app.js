dab36551f85e0a557b0d356c771ed79752f2ba36
RoninXYZ <xyzronin97@gmail.com>
2022-08-28 10:15:22 +0700

    pakai send file

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile('./index.html', { root: __dirname })
+
+
+
+    //   res.json({
+    //     nama :"sandika",
+    //     email :"sandika@gmail.com",
+    //     noHP : 0812345456,
+    //   })
 })
 app.get('/about', (req, res) => {
-    res.send('ini halaman about')
-  })
-  app.get('/kontak', (req, res) => {
-    res.send('ini halaman kontak')
-  })
+    res.sendFile('./about.html', { root: __dirname })
+})
+app.get('/kontak', (req, res) => {
+    //res.send('ini halaman kontak')
+    res.sendFile('./contac.html', { root: __dirname })
+})
+
+app.use('/', (req, res) => {
+    res.status(404);
+    res.send('<h1>404</h1>');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 
